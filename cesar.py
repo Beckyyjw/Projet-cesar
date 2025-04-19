2bcd6717418068632fa91657c805ad03e6395f87
Beckyyjw <beckyy.jw@gmail.com>
2025-04-20 00:42:01 +0200

    Correction du chiffrement César

--- cesar.py
+++ cesar.py
@@ -1,3 +1,25 @@
+uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+lowercase = "abcdefghijklmnopqrstuvwxyz"
 
 def cesar_encrypt(clearText, step):
     cipherText = ""
+
+    for letter in clearText:
+        if letter in uppercase:
+            index = uppercase.index(letter)
+            letter_encrypted = uppercase[(index + step) % 26]
+            cipherText += letter_encrypted
+        elif letter in lowercase:
+            index = lowercase.index(letter)
+            letter_encrypted = lowercase[(index + step) % 26]
+            cipherText += letter_encrypted
+        else:
+            cipherText += letter  # conserve les espaces, ponctuations, etc.
+
+    return cipherText
+
+
+clearText = input("Entrer le texte à chiffrer : ")
+step = int (input("Entrer le pas : "))
+cipherText = cesar_encrypt(clearText, step)
+print(cipherText)
